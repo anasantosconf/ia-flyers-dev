9527988d68dadf3343bae57edfadba142c137b45
anasantosconf <ana.santos@confiseguros.com.br>
2025-12-27 11:15:14 -0300

    teste saveDrive

--- test-save.js
+++ test-save.js
@@ -1,21 +1,6 @@
-import fetch from "node-fetch";
-
-async function testSave() {
-  try {
-    const res = await fetch("http://localhost:3000/api/saveToDrive", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        content: "Teste de flyer automatizado",
-        fileName: "flyer-teste.txt"
-      })
-    });
-
-    const data = await res.json();
-    console.log("Resultado do teste:", data);
-  } catch (err) {
-    console.error("Erro ao testar saveToDrive:", err);
-  }
+export async function GET() {
+  return new Response(
+    JSON.stringify({ ok: true, source: "app-router" }),
+    { status: 200 }
+  );
 }
-
-testSave();
